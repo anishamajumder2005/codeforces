a1ce4d51f853a17501f844892731d842b9e3da89
anishamajumder2005 <146318856+anishamajumder2005@users.noreply.github.com>
2024-12-13 11:09:57 +0530

    Add files via upload

---
+++ sumbinarytree.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        ll ans=0;
+        while(n>0){
+            ans+=n;
+            n/=2;
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
