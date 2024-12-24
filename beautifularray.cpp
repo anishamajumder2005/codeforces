6b928894d3931760ebe55a0a3fe1c249ba253cda
anishamajumder2005 <146318856+anishamajumder2005@users.noreply.github.com>
2024-12-24 19:24:01 +0530

    Add files via upload

---
+++ beautifularray.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){
+    ll a,b;
+    cin>>a>>b;
+    ll x=b;
+    ll y=b;
+    ll z=3*a;
+    z-=(2*b);
+    cout<<3<<endl;
+    cout<<x<<" "<<y<<" "<<z<<" ";
+    return 0;
+}
